a75175fa68aaf112718cd922c664f26be80d9242
ThiagoDiasV <thiago76ers@gmail.com>
2019-11-24 23:46:52 -0300

    secret key bug fix

--- backend/silvertec/settings.py
+++ backend/silvertec/settings.py
@@ -5,14 +5,16 @@ from unipath import Path
 from dj_database_url import parse as db_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = Path(__file__).parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-SECRET_KEY = config('SECRET_KEY') or 'mydemokey'
+try:
+    SECRET_KEY = config('SECRET_KEY')
+except decouple.UndefinedValueError:  # Secret issue, just for demo purpose
+    SECRET_KEY = 'mydemokey'
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 TEMPLATE_DEBUG = DEBUG
