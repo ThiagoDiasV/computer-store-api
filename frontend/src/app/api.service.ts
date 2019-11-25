e1db1c65629474dc47c034455b116c00ecae4d8a
ThiagoDiasV <thiago76ers@gmail.com>
2019-11-24 21:28:27 -0300

    deploy angular

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class ApiService {
 
-  private apiRoot = 'http://localhost:8000/';
+  private apiRoot = 'https://silvertec.herokuapp.com/';
 
   constructor(private http: HttpClient) { }
 
